08db893ddc726a65b234673e92dbbd523fb62fb7
Cloud User <ec2-user@ip-172-31-44-159.ap-south-1.compute.internal>
2021-07-21 18:22:22 +0000

    Updated

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+ver=`curl --silent http://mirror.vorboss.net/apache/tomcat/tomcat-8/ | grep v8 | awk '{split($5,c,">v") ; split(c[2],d,"/") ; print d[1]}'`
 sudo yum install -y java wget
 cd /opt
 rm -rf apache*
-sudo wget https://downloads.apache.org/tomcat/tomcat-9/v9*/bin/apache-tomcat-9*.tar.gz
-sudo tar xvzf apache-tomcat-9.0.48.tar.gz
+sudo wget https://downloads.apache.org/tomcat/tomcat-9/v${ver}/bin/apache-tomcat-${ver}.tar.gz
+sudo tar xvzf apache-tomcat-9.*
 rm -rf apache*.gz
 git clone https://github.com/adhig93/tomcat-config
 cp ./tomcat-config/context.xml /opt/apache-tomcat-9.*/webapps/manager/META-INF/context.xml
