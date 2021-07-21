a7428f73cdd860fe8d61f46b3d5141bdfca52690
adhig93 <81528871+adhig93@users.noreply.github.com>
2021-07-22 00:05:43 +0530

    Update install-tomcat.sh

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -5,11 +5,11 @@ cd /opt
 rm -rf apache*
 sudo wget https://downloads.apache.org/tomcat/tomcat-9/v${ver}/bin/apache-tomcat-${ver}.tar.gz
 sudo tar xvzf apache-tomcat-9.*
-rm -rf apache*.gz
-git clone https://github.com/adhig93/tomcat-config
-cp ./tomcat-config/context.xml /opt/apache-tomcat-9.*/webapps/manager/META-INF/context.xml
-cp ./tomcat-config/context.xml /opt/apache-tomcat-9.*/webapps/host-manager/META-INF/context.xml
-cp ./tomcat-config/tomcat-users.xml /opt/apache-tomcat-9.*/conf/tomcat-users.xml
-rm -rf tomcat-config
+sudo rm -rf apache*.gz
+sudo git clone https://github.com/adhig93/tomcat-config
+sudo cp ./tomcat-config/context.xml /opt/apache-tomcat-9.*/webapps/manager/META-INF/context.xml
+sudo cp ./tomcat-config/context.xml /opt/apache-tomcat-9.*/webapps/host-manager/META-INF/context.xml
+sudo cp ./tomcat-config/tomcat-users.xml /opt/apache-tomcat-9.*/conf/tomcat-users.xml
+sudo rm -rf tomcat-config
 cd apache-tomcat-9.*/bin/
 sh startup.sh
