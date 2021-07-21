4158462311794f888fe070322c26d140a336f3a3
adhig93 <81528871+adhig93@users.noreply.github.com>
2021-07-21 23:42:31 +0530

    Update install-tomcat.sh

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -2,7 +2,7 @@
 sudo yum install -y java wget
 cd /opt
 rm -rf apache*
-sudo wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.48/bin/apache-tomcat-9.0.48.tar.gz
+sudo wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.48/bin/apache-tomcat-9*.tar.gz
 sudo tar xvzf apache-tomcat-9.0.48.tar.gz
 rm -rf apache*.gz
 git clone https://github.com/adhig93/tomcat-config
